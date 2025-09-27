e2182c65e62895069eee2696fa5de55c354f6a3a
mavmodz <gaurav.chauhan@techglide.in>
2025-09-27 15:11:32 +0530

    changed url

--- middleware/admin.ts
+++ middleware/admin.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
     if (!useCookie('token').value) {
-        return navigateTo('https://devcrm.nrichlearning.in', { external: true })
+        return navigateTo('https://dev.nrichlearning.in', { external: true })
         // return navigateTo('/auth/login')
     }
     const data = (await useFetch('/api/users/admin')).data.value;
